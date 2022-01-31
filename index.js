512f6a9f1198640d7d127bc3f28d47da5e7e53ba
Brian Vu <brianvu7@gmail.com>
2022-01-31 16:46:07 -0600

    commit tree 5503756dfd0801afd1775a823b0640548af1923e

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (num) => {
+    if(num > 42){
+        return num - 42;
+    }
+    else{
+        return 42 - num;
+    }
+}
+
+const distanceFromHqInFeet = (blocks) => {
+    if(blocks > 42){
+        return (blocks - 42) * 264;
+    }
+    else{
+        return (42 - blocks) * 264;
+    }
+}
+
+const distanceTravelledInFeet = (num1, num2) => {
+    if(num1 > num2){
+        return (num1 - num2) * 264;
+    }
+    else{
+        return (num2 - num1) * 264;
+    }
+}
+
+const calculatesFarePrice = (start, destination) => {
+    let numBlocksTravelled;
+
+    if(start > destination){
+        numBlocksTravelled = start - destination;
+    }
+    else{
+        numBlocksTravelled = destination - start;
+    }
+
+    let feetTravelled = Math.abs(numBlocksTravelled) * 264 - 400;
+    console.log(feetTravelled)
+
+    if(numBlocksTravelled === 1){
+        return 0;
+    }
+    else if(feetTravelled <= 1600){
+        return feetTravelled * 0.02;
+    }
+    else if(feetTravelled > 1600 && feetTravelled < 2100){
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
